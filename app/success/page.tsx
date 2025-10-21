f706c28451e3a054f72dd3dd01c3ecd22d00da69
Wolff <airpoolfan@gmail.com>
2025-10-22 00:56:50 +0200

    fix: Mark app/success/page.tsx as a Client Component

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect } from 'react';
 import { useGlobalCart } from '@/components/GlobalCartContext';
 import { useSearchParams } from 'next/navigation'; // Import useSearchParams
